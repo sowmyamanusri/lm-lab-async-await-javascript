f3e498c836db200a2b246d379a55c64cf4c193d6
Sowmya <sowmya.manu@yahoo.com>
2022-04-07 20:36:35 +0100

    used async function call to fetch API request and awaited for response in json format

--- exercise002.js
+++ exercise002.js
@@ -2,11 +2,15 @@ import fetch from "node-fetch";
 
 const jsonTypicode = "https://jsonplaceholder.typicode.com/todos/1";
 
-const fetchData = (apiEndPoint) => {
-  fetch(apiEndPoint)
-    .then((response) => response.json())
-    .then((json) => console.log(json))
-    .catch((error) => console.log(error));
+const fetchData = async (apiEndPoint) => {
+  let message = "Catch the error";
+  try {
+    const response = await fetch(apiEndPoint);
+    const result = await response.json();
+    console.log(result);
+  } catch (error) {
+    console.log(error);
+  }
 };
 
 fetchData(jsonTypicode);
