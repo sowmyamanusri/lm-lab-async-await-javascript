5e3c837e7f16e2deccdb16f7a7ee00e8e8be7331
Sowmya <sowmya.manu@yahoo.com>
2022-04-07 20:03:42 +0100

    Implemented async function to call promise ,used try and catch method to handle the response and error

--- exercise001.js
+++ exercise001.js
@@ -1,18 +1,22 @@
-const promise = new Promise((resolve, reject) => {
-  // Generate random number between 0 and 9
-  const randomInt = Math.floor(Math.random() * 10);
+const somePromise = () => {
+  return new Promise((resolve, reject) => {
+    const randomInt = Math.floor(Math.random() * 10);
+    if (randomInt % 2 === 0) {
+      setTimeout(() => resolve('done'), 2000);
+    } else {
+      setTimeout(() => reject('error'), 2000)
+    }
+  })
+}
 
-  if (randomInt % 2 === 0) {
-    // Success
-    setTimeout(() => resolve("done"), 2000);
-  } else {
-    // Failure
-    setTimeout(() => reject("error"), 2000);
+
+
+const getPromiseResponse = async () => {
+  try {
+    const response = await somePromise();
+    console.log(`Yay! Promise resolved with response:${response}`);
+  } catch (error) {
+    console.error(`Boo. Promise rejected with response: ${error}`);
   }
-});
-promise.then((response) => {
-    console.log(`Yay! Promise resolved with response: ${"done"}`);
-  })
-  .catch((error) => {
-    console.log(`Boo. Promise rejected with response: ${"error"}`);
-  })
+}
+getPromiseResponse();
