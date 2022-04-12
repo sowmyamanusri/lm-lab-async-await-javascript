c4ce4a0f23472f68dc08711b6cc7c2f06051fbe3
Sowmya <sowmya.manu@yahoo.com>
2022-04-12 21:02:43 +0100

    passing url to async function and await for response in json format

--- exercise002.js
+++ exercise002.js
@@ -2,10 +2,10 @@ import fetch from "node-fetch";
 
 const jsonTypicode = "https://jsonplaceholder.typicode.com/todos/1";
 
-const fetchData = async (apiEndPoint) => {
+const fetchData = async (jsonTypicode) => {
   let message = "Catch the error";
   try {
-    const response = await fetch(apiEndPoint);
+    const response = await fetch(jsonTypicode);
     const result = await response.json();
     console.log(result);
   } catch (error) {
