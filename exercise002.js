d434f5a8a28bc91aaa274fe0a93273ac85291c57
Sowmya <sowmya.manu@yahoo.com>
2022-04-14 16:11:05 +0100

    updated the error message

--- exercise002.js
+++ exercise002.js
@@ -3,13 +3,13 @@ import fetch from "node-fetch";
 const jsonTypicode = "https://jsonplaceholder.typicode.com/todos/1";
 
 const fetchData = async (jsonTypicode) => {
-  let message = "Catch the error";
+  let message = "Error !! Somwthing went wrong";
   try {
     const response = await fetch(jsonTypicode);
     const result = await response.json();
     console.log(result);
   } catch (error) {
-    console.log(error);
+    console.log(message);
   }
 };
 
